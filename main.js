58a66b95e2c41bf8a418c31d08c6906aa23be282
luiscastro193 <luiscastro193@gmail.com>
2025-07-02 18:30:32 +0200

    Fix order

--- main.js
+++ main.js
@@ -11,6 +11,6 @@ function index(difference) {
 	return ((difference % nMessages) + nMessages) % nMessages;
 }
 
+window.onhashchange = () => location.reload();
 let [, type, date] = location.hash.slice(1).match(/([^\d]+)(\d+)/);
 document.querySelector('img').src = `images/${type}${index(daysSince(date))}.png`;
-window.onhashchange = () => location.reload();
